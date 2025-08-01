38763696470ccd26bf5cc628597dda63d5806bf0
Sunil Darshanam <skdarshanam@snaplogic.com>
2025-08-01 12:58:04 +0530

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,7 +4,7 @@ import { config } from "./env.js";
 const c = config.postgres;
 
 export const databaseUrl = c.connectionString
-    ?? `postgresql://${encodeURIComponent(c.username)}:${encodeURIComponent(c.password)}@${c.host}:${c.port}/${c.database}?sslmode=require`;
+    ?? `postgresql://${encodeURIComponent(c.username)}:${encodeURIComponent(c.password)}@${c.host}:${c.port}/${c.database}`;
 
 export const resourceBaseUrl = new URL(databaseUrl);
 resourceBaseUrl.protocol = "postgres:";
